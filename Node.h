9b705c5f50618520d22e1c135dc04af248b60452
Claire Liu <claire.l.liu1@gmail.com>
2022-09-28 10:29:56 -0400

    minor edits

--- Node.h
+++ Node.h
@@ -8,20 +8,47 @@
 
 using namespace std;
 
+/// @brief represents a state
 class Node {
  public:
+  /// @brief Construct a state
+  /// @param state tile arrangement
+  /// @param parentNode
+  /// @param action the parent took to get to the state
+  /// @param cost estimated cost of this state f(n)
+  /// @param depth g(n) initial state to this state
+  /// @param heuristicEstimate h(n)
   Node(Puzzle* state, Node* parentNode, int action, float cost, int depth, int heuristicEstimate);
 
+  /// @brief getter for state of tile arrangement
+  /// @return
   Puzzle* getState();
+
+  /// @brief getter for parent node
+  /// @return
   Node* getParentNode();
+
+  /// @brief getter for action parent took to get to this state
+  /// @return
   int getAction();
+
+  /// @brief getter for f(n)
+  /// @return
   float getCost();
+
+  /// @brief getter for g(n)
+  /// @return
   int getDepth();
+
+  /// @brief getter for h(n)
+  /// @return
   int getHeuristicEstimate();
 
+  /// @brief print node contents to cout
   void printNode();
 
  private:
+  // instance variables
   Puzzle* state;
   Node* parentNode;
   int action;
